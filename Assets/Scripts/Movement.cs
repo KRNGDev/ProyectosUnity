fcd206035985d670e14caaf9c1c8be7d10e92c79
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-02-28 20:29:20 +0100

    Guarda 28/02

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-   private CharacterController characterController;
+    public Rigidbody rbPajaro;
+    public float potencia = 20;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-    
-        characterController= GetComponent<CharacterController>();
+        rbPajaro = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hori= Input.GetAxis("Horizontal");
-        float ver= Input.GetAxis("Vertical");
-        if(hori!=0|| ver != 0){
+        float hor = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Vector3 positionPlayer = player.position;
 
-        characterController.Move(transform.forward*4*Time.deltaTime);
+        if (positionPlayer.y > 8)
+        {
+            transform.position = new Vector3(positionPlayer.x, 8, positionPlayer.z);
+
         }
+        else if (positionPlayer.y < -2)
+        {
+            transform.position = new Vector3(positionPlayer.x, -2, positionPlayer.z);
+        }
+
+
+
+
+        //transform.Translate(Vector3.forward*Time.deltaTime*10);
+        rbPajaro.AddForce(Vector3.right * hor * 5);
+        rbPajaro.AddForce(Vector3.up * vert * potencia);
+
+
+
+
+
     }
 }
