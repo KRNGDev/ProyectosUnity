a779ab5404fad244330305f661b80ad0e664bae0
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-02-29 12:15:52 +0100

    Inicio en casa

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,7 +6,7 @@ public class Movement : MonoBehaviour
 {
 
     public Rigidbody rbPajaro;
-    public float potencia = 20;
+    public float potencia = 20f;
     public Transform player;
 
     // Start is called before the first frame update
@@ -25,19 +25,26 @@ public class Movement : MonoBehaviour
         if (positionPlayer.y > 8)
         {
             transform.position = new Vector3(positionPlayer.x, 8, positionPlayer.z);
+            rbPajaro.AddForce(Vector3.down*potencia, ForceMode.Impulse);
 
         }
         else if (positionPlayer.y < -2)
         {
             transform.position = new Vector3(positionPlayer.x, -2, positionPlayer.z);
+            rbPajaro.AddForce(Vector3.up*potencia, ForceMode.Impulse);
         }
 
 
 
 
-        //transform.Translate(Vector3.forward*Time.deltaTime*10);
-        rbPajaro.AddForce(Vector3.right * hor * 5);
-        rbPajaro.AddForce(Vector3.up * vert * potencia);
+        
+        rbPajaro.AddForce(Vector3.right * hor*5);
+         
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+
+            rbPajaro.AddForce(Vector3.up *  potencia, ForceMode.Impulse);
+
+        }
 
 
 
